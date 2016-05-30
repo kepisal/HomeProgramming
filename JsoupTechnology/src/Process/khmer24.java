01818376740bd16a01006868c794ce5b913b683f
kepisal <kepisal@gmail.com>
2016-05-30 16:42:48 +0700

    htmlunit
    htmlunit

--- JsoupTechnology/src/Process/khmer24.java
+++ JsoupTechnology/src/Process/khmer24.java
@@ -1,13 +1,31 @@
 package Process;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+ 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.security.cert.X509Certificate;
 
 public class khmer24 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		try {
-			Document doc = Jsoup.connect("http://www.khmer24.com/").get();
-			System.out.println(doc.body());
+//			System.setProperty(key, value)
+			Document doc = Jsoup.connect("https://www.khmer24.com").userAgent("Mozilla").get();
+			Element myelemet  = doc.body();
+			System.out.println(myelemet.select("div.category_block"));
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
